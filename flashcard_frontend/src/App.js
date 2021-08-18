934630fa5407eafb90c0d63a39c783bec65acba6
asiaromaria <designsbyarc@gmail.com>
2021-08-17 17:18:08 -0700

    added title bar.

--- flashcard_frontend/src/App.js
+++ flashcard_frontend/src/App.js
@@ -1,23 +1,53 @@
-// import './App.css';
+import React, { Component } from 'react';
+import TitleBar from './Components/TitleBar/titleBar';
+import axios from 'axios';
+import Deck from './Components/Cards/decks';
+import CardsForm from './Components/Cards/cardsForm';
+import 'bootstrap/dist/css/bootstrap.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+// import CardCarousel from './Cards/cardCarousel';
 
-// export default App;
+class App extends Component {
+    constructor(props){
+        super(props);
+        // this.handlechange = this.handlechange.bind(this);
+        this.state = { 
+            decks: [{name: "", description: "", cardSet: []}],
+        }
+    }
+    
+
+    componentDidMount(){
+    console.log("i mounted");
+    this.makeGetRequest();
+    console.log("i requested");
+    }
+
+    async makeGetRequest() {
+        try {
+            let response = await axios.get(
+                "http://localhost:4000/api/decks/"
+            );
+            console.log(response.data);
+            this.setState({
+                decks: response.data,
+            });
+        }   catch (err) {
+            console.log("Error in API call!", err);
+        }
+    }
+
+    render () {
+        return (
+            <div className="container-fluid"> 
+            <TitleBar />
+            <Deck decks={this.state.decks} />
+            {this.state.decks.cardSet}
+            <CardsForm />
+            {this.makeGetRequest}
+            </div>
+        );
+    }
+}
+
+export default App;
