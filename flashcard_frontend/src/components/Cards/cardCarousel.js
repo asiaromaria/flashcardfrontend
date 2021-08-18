2fd1dfcabab0110a1aed31225dc59efd10407933
asiaromaria <designsbyarc@gmail.com>
2021-08-17 17:20:01 -0700

    imported bootstraps.

---
+++ flashcard_frontend/src/components/Cards/cardCarousel.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// const CardCarousel = (props) => {
+//     return (
+//         <div>
+//             {props.decks.map((cardSet,index) => <h1 key={index}>{cardSet.name}</h1>)}
+//         </div>
+//     )
+// }
+ 
+// export default CardCarousel;
