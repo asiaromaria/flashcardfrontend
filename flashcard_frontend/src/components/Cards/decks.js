8b05f4cec5714a3d97eaf76547d89ea9c5bf969e
asiaromaria <designsbyarc@gmail.com>
2021-08-17 17:19:11 -0700

    i have cards, but took out for now until i figureo out how to make decks like this.

--- flashcard_frontend/src/components/Cards/decks.js
+++ flashcard_frontend/src/components/Cards/decks.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
+
 const Deck = (props) => {
     return (
         <div>
             {props.decks.map((deck,index) => <h1 key={index}>{deck.name}</h1>)}
+            {/* <div class="row">
+                <div class="col-sm-6">
+                    <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="app.js" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="app.js" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+                </div>
+                </div> */}
         </div>
+        
     )
 }
 
